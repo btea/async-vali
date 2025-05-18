b4208f5c3779a1fcc4835baa37d615458d8465e7
Noblet Ouways <91417411+Dsaquel@users.noreply.github.com>
2025-05-18 08:58:48 +0200

    fix: unify string length (#1)
    Co-authored-by: btea <2356281422@qq.com>

--- src/rule/range.ts
+++ src/rule/range.ts
@@ -5,8 +5,6 @@ const range: ExecuteRule = (rule, value, source, errors, options) => {
   const len = typeof rule.len === 'number';
   const min = typeof rule.min === 'number';
   const max = typeof rule.max === 'number';
-  // 正则匹配码点范围从U+010000一直到U+10FFFF的文字（补充平面Supplementary Plane）
-  const spRegexp = /[\uD800-\uDBFF][\uDC00-\uDFFF]/g;
   let val = value;
   let key = null;
   const num = typeof value === 'number';
@@ -25,12 +23,8 @@ const range: ExecuteRule = (rule, value, source, errors, options) => {
   if (!key) {
     return false;
   }
-  if (arr) {
+  if (arr || str) {
     val = value.length;
-  }
-  if (str) {
-    // 处理码点大于U+010000的文字length属性不准确的bug，如"𠮷𠮷𠮷".length !== 3
-    val = value.replace(spRegexp, '_').length;
   }
   if (len) {
     if (val !== rule.len) {
